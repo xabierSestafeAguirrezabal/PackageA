bc796dc1c28f811040c0ffab52fa69ddfd341d09
Xabier Sestafe <xabier.sestafe@ironchip.com>
2022-08-31 16:50:52 +0200

    Initial Commit

---
+++ Sources/PackageA/PackageA.swift
@@ -0,0 +1,9 @@
+public struct PackageA {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+    public func sayPackageA() {
+        print("HELLO PACKAGE A")
+    }
+}
